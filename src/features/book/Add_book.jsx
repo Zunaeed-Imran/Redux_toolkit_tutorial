0d96b4a2b914ae40302c3f1f0a90277a3fb0135d
imran <zunaeede@gmail.com>
2024-04-01 22:15:14 +0600

    main - not done yet.

--- src/features/book/Add_book.jsx
+++ src/features/book/Add_book.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import { title } from 'process';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Add_book = () => {
-  
-  return (
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch()
 
+  const numberofBooks = useSelector((state) => state.booksReducer.books.length);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = { id: numberofBooks + 1, title, author };
+    dispatch(addBook(book))
+  };
+
+  return (
     <>
       <h1>Add Book.</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="title"
+            placeholder="BookTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default Add_book;
