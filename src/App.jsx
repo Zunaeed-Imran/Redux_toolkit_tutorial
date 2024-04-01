1803a1d7290bc4ebcebb4ec9425fb6f18ad414de
imran <zunaeede@gmail.com>
2024-04-01 16:30:54 +0600

    fetch_data - changte heading.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Counter App</h1>
+      <h1>Fetch Data.</h1>
 
       <PostsView/>
     </>
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -4,7 +4,6 @@ import postReducer from '../features/posts/postSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
     posts: postReducer,
   },
 });
