75b63cc74a2ea82dcc50c49235ab8d0a069d18ae
imran <zunaeede@gmail.com>
2024-04-01 19:59:31 +0600

    crud - finally fix hte error in the store file by removing curly bracket.

--- src/features/book/Books_view.jsx
+++ src/features/book/Books_view.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Books_view = () => {
+
+  const books = useSelector(state => state.bookSlice);
 
   return (
 
     <>
       <h1>List of Books</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books && books.map((book) => {
+            const { id, title, author } = book;
+            return <tr key={id}>
+              <td>{id}</td>
+              <td>{title}</td>
+              <td>{author}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </>
   )
 }
